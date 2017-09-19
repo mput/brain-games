7c62c5d7c43f35c39293c9d050de54d970b22b0d
mput <putintsev@gmail.com>
2017-09-19 15:44:28 +0300

    Add game engine. (Not tested

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+const greeter = (rules) => {
   console.log('Welcome to the Brain Games!');
+  if (rules) {
+    console.log(rules);
+  }
+};
+
+const getName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
+
+const getAnswer = (question) => {
+  console.log(`Question: ${question}`);
+  const answer = readlineSync.question('Your answer: ');
+  return answer;
+};
+
+const gameEngine = (qstnGenFn, rightAnswFn, name) => {
+  const runGame = (rightAnswer = 0, shouldBeRight = 3) => {
+    if (rightAnswer === shouldBeRight) {
+      console.log(`Congratulations, ${name}!`);
+      return 'Game over';
+    }
+    const question = qstnGenFn();
+    const userAnswer = getAnswer(question);
+    const rightAnsw = rightAnswFn(question) 
+    if (userAnswer === rightAnsw) {
+      console.log('Correct!');
+      return runGame(rightAnswer + 1);
+    }
+    console.log(`'${userAnswer}' is is wrong answer ;(. Correct answer was '${rightAnsw}'.`);
+    console.log(`Let's try again, ${name}!`);
+    return runGame();
+  };
+  return runGame;
+};
+
+
+export { greeter, getName, gameEngine };
