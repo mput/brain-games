dbb592d19f0a03693d378dd4f0fe874a3c1e963a
mput <putintsev@gmail.com>
2017-09-21 22:02:46 +0300

    Refact calcGame

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,27 +1,25 @@
 import { cons } from 'hexlet-pairs';
 import newGame from '..';
-import { randomInt } from '../utils';
+import randomInt from '../utils';
 
-export default () => {
-  const description = 'What is the result of the expression?';
-  const queAndAnsw = () => {
-    const firstNumber = randomInt(9);
-    const secondNumber = randomInt(9);
-    const operator = ['+', '-', '*'][randomInt(3) - 1];
-    const question = `${firstNumber} ${operator} ${secondNumber}`;
-
-    const findAnswer = (sign) => {
-      switch (sign) {
-        case '+':
-          return firstNumber + secondNumber;
-        case '-':
-          return firstNumber - secondNumber;
-        default:
-          return firstNumber * secondNumber;
-      }
-    };
-    const answer = String(findAnswer(operator));
-    return cons(question, answer);
-  };
-  return newGame(description, queAndAnsw);
+const description = 'What is the result of the expression?';
+const findAnswer = (operator, firstNumber, secondNumber) => {
+  switch (operator) {
+    case '+':
+      return firstNumber + secondNumber;
+    case '-':
+      return firstNumber - secondNumber;
+    default:
+      return firstNumber * secondNumber;
+  }
 };
+const getQueAndAnsw = () => {
+  const firstNumber = randomInt(9);
+  const secondNumber = randomInt(9);
+  const operator = ['+', '-', '*'][randomInt(3) - 1];
+  const question = `${firstNumber} ${operator} ${secondNumber}`;
+  const answer = findAnswer(operator, firstNumber, secondNumber);
+  return cons(question, String(answer));
+};
+
+export default () => newGame(description, getQueAndAnsw);
