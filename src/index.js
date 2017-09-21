efcb5eedeba59eb34f3a8da42a13c0eceaa1de42
mput <putintsev@gmail.com>
2017-09-21 21:47:24 +0300

    Refact game engine

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
-const newGame = (rules, queAndAnsw) => {
+const stepsAmount = 3;
+
+const runGame = (getQueAndAnsw, currentStep = 0) => {
+  if (currentStep === stepsAmount) {
+    return true;
+  }
+  const newQueAndAnsw = getQueAndAnsw();
+  const question = car(newQueAndAnsw);
+  const rightAnswer = cdr(newQueAndAnsw);
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (userAnswer === rightAnswer) {
+    console.log('Correct!');
+    return runGame(getQueAndAnsw, currentStep + 1);
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+  return false;
+};
+
+export default (description, getQueAndAnsw) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(rules);
-  const runGame = (currentStep) => {
-    const gameStepsAmount = 3;
-    if (currentStep === gameStepsAmount) {
-      console.log(`Congratulations, ${userName}!`);
-      return 0;
-    }
-    const newQueAndAnsw = queAndAnsw();
-    const question = car(newQueAndAnsw);
-    const rightAnswer = cdr(newQueAndAnsw);
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === rightAnswer) {
-      console.log('Correct!');
-      return runGame(currentStep + 1);
-    }
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
-    console.log(`Let's try again, ${userName}!`);
-    return runGame(0);
-  };
-  const currentStep = 0;
-  return runGame(currentStep);
+  console.log(description);
+  const isGameWin = runGame(getQueAndAnsw)
+  const byeMessage = isGameWin ?
+    `Congratulations, ${userName}!` :
+    `Let's try again, ${userName}!`;
+  console.log(byeMessage);
 };
-
-export default newGame;
