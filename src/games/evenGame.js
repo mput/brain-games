bd602ed25caeb9fe986d55284f3d37541012c2e9
mput <putintsev@gmail.com>
2017-09-21 21:57:06 +0300

    Refact game evenGame

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -2,12 +2,11 @@ import { cons } from 'hexlet-pairs';
 import newGame from '..';
 import { randomInt } from '../utils';
 
-export default () => {
-  const description = 'Answer "yes" if number even otherwise answer "no"';
-  const queAndAnsw = () => {
-    const question = randomInt(50);
-    const answer = ((question % 2 === 0) ? 'yes' : 'no');
-    return cons(String(question), answer);
-  };
-  return newGame(description, queAndAnsw);
+const description = 'Answer "yes" if number even otherwise answer "no"';
+const getQueAndAnsw = () => {
+  const question = randomInt(50);
+  const answer = ((question % 2 === 0) ? 'yes' : 'no');
+  return cons(String(question), answer);
 };
+
+export default () => newGame(description, getQueAndAnsw);
