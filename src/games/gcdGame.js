021d2ee9ea338f2acd6872fe717947f90922313f
mput <putintsev@gmail.com>
2017-09-21 16:57:40 +0300

    Fix gcdGame for new structure

--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -1,20 +1,15 @@
+import { cons } from 'hexlet-pairs';
 import newGame from '..';
 import { randomInt, findGcd } from '../utils';
 
 export default () => {
-  const rules = 'Find the greatest common divisor of given numbers.';
-  const newQuestion = () => {
+  const description = 'Find the greatest common divisor of given numbers.';
+  const queAndAnsw = () => {
     const firstNumber = randomInt(20);
     const secondNumber = randomInt(20);
-    return [firstNumber, secondNumber];
+    const question = `${firstNumber} ${secondNumber}`;
+    const answer = findGcd(firstNumber, secondNumber);
+    return cons(question, String(answer));
   };
-
-  const answer = (question) => {
-    const firstNumber = question[0];
-    const secondNumber = question[1];
-    return String(findGcd(firstNumber, secondNumber));
-  };
-
-  const questionToString = question => `${question[0]} ${question[1]}`;
-  return newGame(rules, newQuestion, questionToString, answer);
+  return newGame(description, queAndAnsw);
 };
