79bdc96de346f2fcf999cb6e7a79fe31b52d977c
mput <putintsev@gmail.com>
2017-09-22 22:29:35 +0300

    Var renaiming

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -5,23 +5,23 @@ import randomInt from '../utils';
 const description = 'What number is missing in this progression?';
 
 const getQueAndAnsw = () => {
-  const progElementsCount = 10;
+  const progElmsCount = 10;
   const skippedElmPos = randomInt(10) - 1;
   const firstElm = randomInt(10);
   const step = randomInt(5);
 
-  const iterQnA = (currentElem, currentElmPos, progression, hiddenElm) => {
-    if (currentElmPos === progElementsCount) {
+  const iterQnA = (currentElm, currentElmPos, progression, hiddenElm) => {
+    if (currentElmPos === progElmsCount) {
       return cons(progression, hiddenElm);
     }
-    const nextElm = currentElem + step;
+    const nextElm = currentElm + step;
     const nextElmPos = currentElmPos + 1;
     const separator = (currentElmPos === 0) ? '' : ' ';
 
     if (currentElmPos === skippedElmPos) {
-      return iterQnA(nextElm, nextElmPos, `${progression}${separator}..`, String(currentElem));
+      return iterQnA(nextElm, nextElmPos, `${progression}${separator}..`, String(currentElm));
     }
-    return iterQnA(nextElm, nextElmPos, `${progression}${separator}${currentElem}`, hiddenElm);
+    return iterQnA(nextElm, nextElmPos, `${progression}${separator}${currentElm}`, hiddenElm);
   };
 
   return iterQnA(firstElm, 0, '', '');
