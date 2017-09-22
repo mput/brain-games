85584c11648c0b5a37a2b08c846196b86dec03f5
mput <putintsev@gmail.com>
2017-09-22 15:05:17 +0300

    Refact calcGame

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -3,22 +3,23 @@ import newGame from '..';
 import randomInt from '../utils';
 
 const description = 'What is the result of the expression?';
-const findAnswer = (operator, firstNumber, secondNumber) => {
-  switch (operator) {
-    case '+':
-      return firstNumber + secondNumber;
-    case '-':
-      return firstNumber - secondNumber;
-    default:
-      return firstNumber * secondNumber;
-  }
-};
+
 const getQueAndAnsw = () => {
   const firstNumber = randomInt(9);
   const secondNumber = randomInt(9);
   const operator = ['+', '-', '*'][randomInt(3) - 1];
   const question = `${firstNumber} ${operator} ${secondNumber}`;
-  const answer = findAnswer(operator, firstNumber, secondNumber);
+  let answer;
+  switch (operator) {
+    case '+':
+      answer = firstNumber + secondNumber;
+      break;
+    case '-':
+      answer = firstNumber - secondNumber;
+      break;
+    default:
+      answer = firstNumber * secondNumber;
+  }
   return cons(question, String(answer));
 };
 
